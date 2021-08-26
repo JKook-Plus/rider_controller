8cd20bc32ccb612cc193f3157803b5d34f953d60
JKook <JKookaburraSchool@gmail.com>
2021-08-26 17:09:43 +1000

    Removed a lot of non-required code Changed quite a few variable names (preparing for asyncio)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import cv2
 from assets.viewer import AndroidViewer
 import assets.keycodes as keycodes
 
+import asyncio
 
 # from tkinter import *
 
@@ -26,6 +27,19 @@ coords = []
 
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+android = AndroidViewer()
+
+
+# Initial score will be 0
+score = 0
+
+
+
+# main loop constants
+resize_size = 10
+white = [[0,0,253], [255,10,255]]
+
 
 
 
@@ -83,108 +97,21 @@ def calcPercentage(msk):
 def close_event():
     plt.close()
 
-android = AndroidViewer()
 
-ep = []
 
-is_dead_list = []
-
-with open('config_text.txt', 'r') as content_file:
-    content = content_file.read()
+# with open('config_text.txt', 'r') as content_file:
+#     content = content_file.read()
 
 
 
-def video_stream():
 
-    resize_size = 10
+def get_score():
+    global score
+
+
+def game_loop(ticker):
 
     frames = android.get_next_frames()
-
-    # print(android.resolution)
-
-    all_color = [[0,0,0], [255,255,255]]
-
-    purple = [[135,25,200], [150,50,255]]
-    green = [[65,25,100], [70,35,255]]
-    red = [[175,100,0], [180,255,255]]
-    pink = [[150,25,200], [160,50,255]]
-    yellow = [[20,0,200], [30,50,255]]
-    cyan = [[80,0,100], [90,50,255]]
-    yellow_2 = [[25,25,100], [40,50,255]]
-    green_2 = [[50,0,0], [60,100,255]]
-    white = [[0,0,253], [255,10,255]]
-
-    wheels = [[0,0,225], [150,10,255]]
-
-    # 1440, 3040
-
-    ground_before = {
-    "Ground":
-                [[[0, 3040], [0, 1440]],
-                purple],
-    # "Ground 2":
-    #             [[[0, 3040], [0, 1440]],
-    #             pink],
-    # "Ground 3":
-    #             [[[0, 3040], [0, 1440]],
-    #             cyan],
-    # "Ground 4":
-    #             [[[0, 3040], [0, 1440]],
-    #             green_2],
-                }
-
-    gam_before = {
-    "Gems and Movables":
-                [[[0, 3040], [0, 1440]],
-                green],
-    # "Gems and Movables 2":
-    #             [[[0, 3040], [0, 1440]],
-    #             yellow],
-    # "Gems and Movables 3":
-    #             [[[0, 3040], [0, 1440]],
-    #             yellow_2],
-    }
-
-
-
-    tot_before = {
-    "Ground":
-                [[[0, 3040], [0, 1440]],
-                purple],
-    "Gems and Movables":
-                [[[0, 3040], [0, 1440]],
-                green],
-    "Wheels":
-                [[[0, 3040], [0, 1440]],
-                wheels],
-    "Dangers":
-                [[[0, 3040], [0, 1440]],
-                red],
-    # "Ground 2":
-    #             [[[0, 3040], [0, 1440]],
-    #             pink],
-    "Gems and Movables 2":
-                [[[0, 3040], [0, 1440]],
-                yellow],
-    # "Ground 3":
-    #             [[[0, 3040], [0, 1440]],
-    #             cyan],
-    "Gems and Movables 3":
-                [[[0, 3040], [0, 1440]],
-                yellow_2],
-    # "Ground 4":
-    #             [[[0, 3040], [0, 1440]],
-    #             green_2],
-                }
-
-
-    # ground = format_masks(ground_before, resize_size)
-
-
-
-    # tot = {}
-    # for key in tot_before:
-    #     tot[key] = [[(np.multiply(tot_before[key][0][0], resize_size/100)).astype(int), (np.multiply(tot_before[key][0][1], resize_size/100)).astype(int)], tot_before[key][1]]
 
     if frames is None:
         pass
@@ -198,7 +125,7 @@ def video_stream():
 
             hor = 400
 
-            score = frame[600:(3040-2300), hor:(1440-hor)]
+            score = frame[580:(3040-2300), hor:(1440-hor)]
 
             # score = resizer(score, resize_size)
 
@@ -209,8 +136,6 @@ def video_stream():
             # print(imS.shape[0], imS.shape[1])
             sc = cv2.cvtColor(imS, cv2.COLOR_BGR2RGB)
             percentages = []
-
-            ground_mask_holder = []
 
             # for key in ground:
             #     x, y = ground[key][0]
@@ -228,11 +153,9 @@ def video_stream():
             # g_1 = cv2.add(mask_holder[0], mask_holder[1])
             # # print(g_1)
             # cv2.imshow("G_1", g_1)
-# mask_holder[0], mask_holder[1], mask_holder[4], mask_holder[5], mask_holder[6], mask_holder[7]
+            # mask_holder[0], mask_holder[1], mask_holder[4], mask_holder[5], mask_holder[6], mask_holder[7]
 
-            g_2 = 0
-            for i in ground_mask_holder:
-                g_2 = cv2.add(g_2, i)
+
 
             edges = cv2.Canny(imS,100,1000)
 
@@ -242,7 +165,6 @@ def video_stream():
             white_perc = calcPercentage(edges)
 
 
-
             if white_perc < 0.6:
                 is_dead = 8
                 print("DEAD")
@@ -250,8 +172,6 @@ def video_stream():
                 is_dead = 0
 
             edges_height, edges_width = edges.shape
-
-            # 284 144
 
             # print(edges_height, edges_width)
 
@@ -265,42 +185,67 @@ def video_stream():
 
             # score
 
+            # score = (255-score)
+
+            blur_amount = 11
+
+            score = cv2.cvtColor(score, cv2.COLOR_BGR2RGB)
+
+            score = cv2.GaussianBlur(score,(blur_amount,blur_amount),cv2.BORDER_DEFAULT)
+
+
+            score = cv2.threshold(score,127,255,cv2.THRESH_BINARY)[1]
 
 
 
-            # text = pytesseract.image_to_string(score, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789+')
-            # print(text)
-
-            # ep.append(white_perc)
-            # is_dead_list.append(is_dead)
+            # score = Image.frombytes('RGB', score.shape[:2], score, 'raw', 'BGR', 0, 0)
 
 
-            # if len(ep)%500 == 0:
-            #     # fig, (ax1, ax2) = plt.subplots(2)
-            #     # fig.suptitle('Is Dead checks')
-            #     plt.plot(ep)
-            #     plt.plot(is_dead_list)
-            #
-            #     # plt.plot(ep)
-            #
-            #     plt.show()
-            #     # ep = []
-            #     # is_dead_list = []
 
 
-            # g_2 = cv2.add(cv2.add((cv2.add(mask_holder[0], mask_holder[1])), (cv2.add(mask_holder[4], mask_holder[5]))), (cv2.add(mask_holder[6], mask_holder[7])))
-            # # g_2 = g_2.clip(0,255).astype("uint8")
+            # --psm 7, 8 and 9 work, 7 was picked at random
 
-            # cv2.imshow("G_2", g_2)
+            if ticker%10==0:
+                text = pytesseract.image_to_string(score, lang='1', config='--psm 8 --oem 3 -c tessedit_char_whitelist=0123456789+').replace("\n", "").replace("\x0c", "")
+                print(text)
+                cv2.imwrite("attempts/1/attempt_1_-%s-_%s.png"%(text, ticker), score)
+                with open("sample.txt", "a") as file_object:
+                    # Append 'hello' at the end of file
+                    file_object.write(text + "\n")
+
+            # print("Text: %s\n      %s"%(text, pytesseract.image_to_string(score)))
 
 
             cv2.imshow('Phone Viewer', resizer(imS, 300))
-            cv2.imshow("Score", resizer(score, 100))
+            cv2.imshow("Score", score)
             cv2.waitKey(1)
 
 
-    # lmain.after(1, video_stream)
 
 
-while True:
-    video_stream()
+
+def video_stream(ticker):
+    pass
+
+
+
+
+    # print(android.resolution)
+
+
+
+
+    # tot = {}
+    # for key in tot_before:
+    #     tot[key] = [[(np.multiply(tot_before[key][0][0], resize_size/100)).astype(int), (np.multiply(tot_before[key][0][1], resize_size/100)).astype(int)], tot_before[key][1]]
+
+
+if __name__ == "__main__":
+
+    ticker = 0
+
+    # android.set_screen_power_mode(0)
+
+    while True:
+        game_loop(ticker)
+        ticker = ticker + 1
